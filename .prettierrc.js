030a8c0c2650a954183a63e85a6fd4c3c491cb92
morrison <morrison.k@hpcnt.com>
2024-06-17 11:10:47 +0900

    초기 세팅

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  printWidth: 200,
+  endOfLine: 'auto',
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'all',
+  bracketSpacing: true,  
+  jsxSingleQuote: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  parser: 'typescript',
+}
